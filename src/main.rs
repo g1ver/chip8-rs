83a714231867155cccfbc3b54343c2e2725c4eae
g1ver <c0ping@pm.me>
2026-02-17 15:33:46 -0800

    setup cpu, memory, and font loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+const MEMORY_SIZE: usize = 4096;
+const INSTRUCTIONS_PER_SEC: usize = 700;
+
+const FONTS: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+struct Chip8 {
+    memory: [u8; MEMORY_SIZE],
+    frame_buffer: [u8; WIDTH * HEIGHT],
+    program_counter: u16,
+    index_register: u16,
+    stack: [u16; 12],
+    stack_pointer: u8, // points at next empty slot
+    // V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF
+    // VF is the flag register
+    registers: [u8; 16],
+    sound_timer: u8,
+    delay_timer: u8,
+}
+
+impl Chip8 {
+    fn new() -> Self {
+        let mut chip8 = Self {
+            memory: [0u8; MEMORY_SIZE],
+            frame_buffer: [0u8; WIDTH * HEIGHT],
+            program_counter: 0x200,
+            index_register: 0x0,
+            stack: [0u16; 12],
+            stack_pointer: 0,
+            registers: [0u8; 16],
+            sound_timer: 0,
+            delay_timer: 0,
+        };
+
+        chip8.load_fonts();
+        chip8
+    }
+
+    fn load_fonts(&mut self) {
+        // Community convention is to load fonts into 0x050 to 0x09F
+        self.memory[0x050..=0x09F].copy_from_slice(&FONTS[..]);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let chip8 = Chip8::new();
 }
